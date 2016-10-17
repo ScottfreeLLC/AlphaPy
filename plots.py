fix threshold calculation
fix threshold calculation

plots.py
@@ -464,7 +464,8 @@ def plot_confusion_matrix(model, partition):
         plt.yticks(tick_marks, y_values)
         # normalize confusion matrix
         cmn = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        thresh = cm.max() / 2.0
+        # place text in square of confusion matrix
+        thresh = (cm.max() + cm.min()) / 2.0
         for i, j in product(range(cm.shape[0]), range(cm.shape[1])):
             cmr = round(cmn[i, j], 3)
             plt.text(j, i, cmr,