ee2879e79645f06b334e3a0c6f0ff7e6b80e3428
Mark Conway <Mark.R.Conway@gmail.com>
2017-04-29 17:08:44 -0400

    update names
    update names

--- docs/conf.py
+++ docs/conf.py
@@ -54,7 +54,7 @@ master_doc = 'index'
 # General information about the project.
 project = 'AlphaPy'
 copyright = '2017, ScottFree Analytics LLC'
-author = 'Mark Conway, Robert D. Scott II'
+author = 'Robert D. Scott II, Mark Conway'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
