d5ebc6aed1ae3648471914f612c7d65b35d1506c
Mark Conway <Mark.R.Conway@gmail.com>
2017-04-30 18:57:49 -0400

    readthedocs is so finicky
    readthedocs is so finicky

--- docs/conf.py
+++ docs/conf.py
@@ -17,10 +17,9 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../'))
+#import os
+#import sys
+#sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
