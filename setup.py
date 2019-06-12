02f9b331dc47e0ba6ba1e98b542f60c5b9c82dc1
Mark Conway <Mark.R.Conway@gmail.com>
2019-06-12 12:35:39 -0400

    version 2.3.8
    version 2.3.8

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ MAINTAINER = 'ScottFree LLC [Robert D. Scott II, Mark Conway]'
 MAINTAINER_EMAIL = 'scottfree.analytics@scottfreellc.com'
 URL = "https://github.com/ScottFreeLLC/AlphaPy"
 LICENSE = "Apache License, Version 2"
-VERSION = "2.3.7"
+VERSION = "2.3.8"
 
 classifiers = ['Development Status :: 4 - Beta',
                'Programming Language :: Python',
